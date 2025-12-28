44edb47186912881ae9c040607f11c625cad1ba8
naokihaba <naosprintrunner6385@gmail.com>
2025-12-28 18:40:30 +0900

    fix: 一貫性のために README.md のリスト形式を修正

--- 13-roman-to-integer/solution.ts
+++ 13-roman-to-integer/solution.ts
@@ -49,13 +49,13 @@
  */
 
 const romanValues: Record<string, number> = {
-  'I': 1,
-  'V': 5,
-  'X': 10,
-  'L': 50,
-  'C': 100,
-  'D': 500,
-  'M': 1000,
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
 };
 
 export function romanToInt(s: string): number {
@@ -74,7 +74,7 @@ export function romanToInt(s: string): number {
     }
 
     prevValue = value;
-  }   
+  }
 
   return total;
 }
